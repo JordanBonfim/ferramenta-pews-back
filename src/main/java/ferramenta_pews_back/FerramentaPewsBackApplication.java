7b2bfab44dcefc7bf40c844e2e12bbe76f52eb75
Jordan <jordanduzabonfim@gmail.com>
2025-01-15 10:40:17 -0300

    Commit inicial

---
+++ src/main/java/ferramenta_pews_back/FerramentaPewsBackApplication.java
@@ -0,0 +1,13 @@
+package ferramenta_pews_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FerramentaPewsBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FerramentaPewsBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ferramenta_pews_back/FerramentaPewsBackApplicationTests.java
@@ -0,0 +1,13 @@
+package ferramenta_pews_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FerramentaPewsBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
